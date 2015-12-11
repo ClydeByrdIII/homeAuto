cbdb6a265d37b2375dcddc533833a9a6fd97cb01
Clyde Byrd III <clydebyrdiii@yahoo.com>
2015-12-10 22:42:14 -0500

    new ads

--- AdafruitDHT.py
+++ AdafruitDHT.py
@@ -68,8 +68,8 @@ if __name__ == '__main__':
     	while did_rec == False:
     		did = register_client(ip, name)
     		if did != '':
-    	       did_rec = True
-    	       print did
+                did_rec = True
+                print did
     	# Try to grab a sensor reading.  Use the read_retry method which will retry up
     	# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
     	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
@@ -82,8 +82,8 @@ if __name__ == '__main__':
     	# guarantee the timing of calls to read the sensor).  
     	# If this happens try again!
     	if humidity is not None and temperature is not None:
-    		msg = 'Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity)
-    		print msg
+    	    msg = 'Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity)
+    	    print msg
     		if temperature > 70:
     			notify_client(ip, did, name, msg)
     	time.sleep(10)
