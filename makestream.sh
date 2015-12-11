30e27c2e83814ee12f97de15d2f8b9f6814571c2
clyde byrd <clydebyrdiii@yahoo.com>
2015-12-11 10:13:54 +0000

    make file

---
+++ makestream.sh
@@ -0,0 +1,1 @@
+raspivid --verbose --nopreview -hf -vf -g 4 --width 640 --height 480 --framerate 15 --bitrate 1500000 --profile baseline --timeout 0 -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! udpsink host=127.0.0.1 port=8004
